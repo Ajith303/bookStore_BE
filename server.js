2dc9f1a2964c61ec62a072c960b6c89d60a18b12
Ajith303 <149502704+Ajith303@users.noreply.github.com>
2026-02-11 15:00:47 +0530

    commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const swaggerJsDocs = yaml.load("./yaml/api.yaml")
 const userRoute = require ("./route/userRoute")
 const productRoute = require("./route/productRoute")
 const contactRoute = require ("./route/contactRoute")
-const AuthMiddleWare = require ("./MiddleWare/AuthMiddlleWare")
+// const AuthMiddleWare = require ("./MiddleWare/AuthMiddlleWare")
 
 const corsOptions = {
      origin:"*",
@@ -29,12 +29,14 @@ app.use(bodyParser.json())
 app.use(morgan("dev"))
 app.use(cors(corsOptions))  
 app.use("/docs",swaggerUI.serve,swaggerUI.setup(swaggerJsDocs))
-app.use(AuthMiddleWare)           
+// app.use(AuthMiddleWare)           
 
 app.use("/user",userRoute)
 app.use("/product",productRoute)
 app.use("/contact",contactRoute)
-
+app.get('/',async(req,res)=>{
+    req.get("Api is running")
+})
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("mongoose connected"))
 .catch(err=>console.log(err))
